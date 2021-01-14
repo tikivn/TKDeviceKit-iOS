df65a8179b240de69a4e2ed733df436aa0a32c28
Duc Nguyen <duc.nguyen@tiki.vn>
2021-01-14 14:51:53 +0700

    Initial commit

---
+++ Example/TKDeviceKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TKDeviceKit
+//
+//  Created by Duc Nguyen on 01/14/2021.
+//  Copyright (c) 2021 Duc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
